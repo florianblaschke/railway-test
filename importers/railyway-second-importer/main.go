6c89e193ae223825f6f47e07260fef3ec12a192f
Florian <127996345+florianblaschke@users.noreply.github.com>
2026-01-09 10:41:56 +0100

    add another importer

---
+++ importers/railyway-second-importer/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/forsuxess/railway-test/jobs"
+	"github.com/sirupsen/logrus"
+)
+
+func init() {
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+}
+
+func main() {
+	job := jobs.NewJob()
+	job.ParseTitleFromString("  Junior Software Engineer (m/f/d)  ")
+
+	logrus.WithFields(logrus.Fields{
+		"client":  "railway_test_client_two",
+		"run_id":  1,
+		"job_id":  301,
+		"title":   job.Title,
+		"success": true,
+	}).Info("railway importer completed")
+}
